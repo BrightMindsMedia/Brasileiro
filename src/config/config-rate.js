c51dcbc47e2d9e5b805a47e90cece7c3d1763bef
Tauser <tauser@gmail.com>
2021-01-21 17:50:45 -0300

    alteracao no config-rate.js

--- src/config/config-rate.js
+++ src/config/config-rate.js
@@ -1,5 +1,5 @@
 export default {
-  AppleAppID: '1475075333',
+  AppleAppID: '1545180255',
   GooglePackageName: 'uk.co.brasileiroonline.mobile',
   preferInApp: true,
   openAppStoreIfInAppFails: true,
